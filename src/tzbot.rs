f8316c6672a92eace713a72a0fef15defa2a3b50
Nogamara <armagonlive@gmail.com>
2022-06-09 22:36:04 +0200

    cargo fmt

--- src/tzbot.rs
+++ src/tzbot.rs
@@ -5,14 +5,20 @@ extern crate hourglass;
 extern crate regex;
 extern crate time;
 
-
 use hourglass::Timezone;
 use regex::Regex;
 use std::collections::HashMap;
 use std::option::Option;
 use std::vec::Vec;
 
-fn format_output(tz: String, hours: String, minutes: String, ampm: String, lookup: HashMap<String, String>, common: Vec<String>) -> String {
+fn format_output(
+    tz: String,
+    hours: String,
+    minutes: String,
+    ampm: String,
+    lookup: HashMap<String, String>,
+    common: Vec<String>,
+) -> String {
     let mut h_i = hours.parse::<i32>().unwrap();
     let m_i = minutes.parse::<i32>().unwrap();
 
@@ -24,7 +30,17 @@ fn format_output(tz: String, hours: String, minutes: String, ampm: String, looku
     let local = Timezone::new(tz_full).unwrap();
 
     let now = time::now();
-    let t_input = local.datetime(1900 + now.tm_year, 1 + now.tm_mon, now.tm_mday, h_i, m_i, 0, 0).unwrap();
+    let t_input = local
+        .datetime(
+            1900 + now.tm_year,
+            1 + now.tm_mon,
+            now.tm_mday,
+            h_i,
+            m_i,
+            0,
+            0,
+        )
+        .unwrap();
 
     let mut result = format!("{}:{}{} {} is ", hours, minutes, ampm, tz);
     for current in common {
@@ -37,7 +53,6 @@ fn format_output(tz: String, hours: String, minutes: String, ampm: String, looku
         println!("DBG:  {}: {:?}", current, cur_t);
         let hm = cur_t.format("%H:%M").unwrap();
         result = format!("{}{} {} / ", result, hm, current);
-
     }
     result.pop();
     result.pop();
@@ -51,41 +66,54 @@ pub fn convert(body_in: &str) -> Option<String> {
 
     let mut slookup = HashMap::new();
 
-    slookup.insert("EST",        "America/New_York");
-    slookup.insert("EASTERN",    "America/New_York");
-    slookup.insert("CST",        "America/Chicago");
-    slookup.insert("CENTRAL",    "America/Chicago");
-    slookup.insert("MST",        "America/Denver");
-    slookup.insert("MOUNTAIN",   "America/Denver");
-    slookup.insert("PST",        "America/Los_Angeles");
-    slookup.insert("PACIFIC",    "America/Los_Angeles");
-    slookup.insert("CET",        "Europe/Berlin");
-    slookup.insert("MSK",        "Europe/Moscow");
-    slookup.insert("GMT",        "UTC");
-    slookup.insert("UTC",        "UTC");
+    slookup.insert("EST", "America/New_York");
+    slookup.insert("EASTERN", "America/New_York");
+    slookup.insert("CST", "America/Chicago");
+    slookup.insert("CENTRAL", "America/Chicago");
+    slookup.insert("MST", "America/Denver");
+    slookup.insert("MOUNTAIN", "America/Denver");
+    slookup.insert("PST", "America/Los_Angeles");
+    slookup.insert("PACIFIC", "America/Los_Angeles");
+    slookup.insert("CET", "Europe/Berlin");
+    slookup.insert("MSK", "Europe/Moscow");
+    slookup.insert("GMT", "UTC");
+    slookup.insert("UTC", "UTC");
 
     // DST
-    slookup.insert("EDT",        "America/New_York");
-    slookup.insert("CDT",        "America/Chicago");
-    slookup.insert("MDT",        "America/Denver");
-    slookup.insert("PDT",        "America/Los_Angeles");
-    slookup.insert("CEST",       "Europe/Berlin");
+    slookup.insert("EDT", "America/New_York");
+    slookup.insert("CDT", "America/Chicago");
+    slookup.insert("MDT", "America/Denver");
+    slookup.insert("PDT", "America/Los_Angeles");
+    slookup.insert("CEST", "Europe/Berlin");
 
     let mut lookup = HashMap::new();
     for (key, val) in slookup.iter() {
         lookup.insert(key.to_string(), val.to_string());
     }
 
-    let mut shorts = lookup.keys().fold(String::new(), |acc, k| { acc + k + "|" });
+    let mut shorts = lookup.keys().fold(String::new(), |acc, k| acc + k + "|");
     shorts.pop();
-    let common = vec!["PST".to_string(), "CST".to_string(), "EST".to_string(), "CET".to_string()];
+    let common = vec![
+        "PST".to_string(),
+        "CST".to_string(),
+        "EST".to_string(),
+        "CET".to_string(),
+    ];
 
     // (\d+):(\d+) (EST|CET|...)
     let re_fmt_base = format!("{} ({})", r"^(.*?)(\d+):(\d+)", shorts.to_uppercase());
     // (\d+):(\d+)(AM|PM| AM| PM) (EST|CET|...)
-    let re_fmt_ampm = format!("{} ({})", r"^(.*?)(\d+):(\d+)(AM|PM| AM| PM)", shorts.to_uppercase());
+    let re_fmt_ampm = format!(
+        "{} ({})",
+        r"^(.*?)(\d+):(\d+)(AM|PM| AM| PM)",
+        shorts.to_uppercase()
+    );
     // (\d{1,3})(AM|PM| AM| PM) (EST|CET|...)
-    let re_fmt_ampm_4d = format!("{} ({})", r"^(.*?)(\d{1,4})(AM|PM| AM| PM)", shorts.to_uppercase());
+    let re_fmt_ampm_4d = format!(
+        "{} ({})",
+        r"^(.*?)(\d{1,4})(AM|PM| AM| PM)",
+        shorts.to_uppercase()
+    );
 
     let re_base = Regex::new(&re_fmt_base).unwrap();
     let re_ampm = Regex::new(&re_fmt_ampm).unwrap();
@@ -136,7 +164,14 @@ pub fn convert(body_in: &str) -> Option<String> {
         return None;
     }
     if lookup.contains_key(&tz) {
-        result = format_output(tz, hours.to_string(), minutes.to_string(), ampm, lookup, common);
+        result = format_output(
+            tz,
+            hours.to_string(),
+            minutes.to_string(),
+            ampm,
+            lookup,
+            common,
+        );
     } else {
         println!("TODO: Timezone not implemented: {}", tz);
         result = format!("Unknown timezone: {}", tz);
@@ -144,4 +179,3 @@ pub fn convert(body_in: &str) -> Option<String> {
 
     Some(result)
 }
-
