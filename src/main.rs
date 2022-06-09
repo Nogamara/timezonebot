35387341479e12fc6585c3b9dc9d38f49d5dcac6
Nogamara <armagonlive@gmail.com>
2022-06-09 22:34:19 +0200

    Bump deps and fix everything

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ extern crate serenity;
 extern crate time;
 extern crate tzbot;
 
-use serenity::CACHE;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
 use serenity::prelude::*;
-use serenity::model::*;
 use std::env;
 use tzbot::convert;
 
@@ -33,16 +34,17 @@ fn now() -> String {
     }
 }
 
+#[async_trait]
 impl EventHandler for Handler {
-    fn on_message(&self, _: Context, msg: Message) {
+    async fn message(&self, context: Context, msg: Message) {
         // don't talk to yourself, bot
-        if msg.author.id == CACHE.read().unwrap().user.id {
+        if msg.is_own(&context.cache) {
             log!("SELF:[{}] | chan:{}", msg.content, msg.channel_id);
             return;
         }
 
         if msg.content == "!tzbot" {
-            if let Err(why) = msg.channel_id.say("Pong!") {
+            if let Err(why) = msg.channel_id.say(&context.http, "Pong!").await {
                 log!("Error sending message: {:?}", why);
             }
         }
@@ -51,37 +53,46 @@ impl EventHandler for Handler {
         match result {
             None => {
                 log!("NOPE:[{}] user:[{}]", msg.content, msg.author.name);
-            },
+            }
             Some(val) => {
-                if let Err(why) = msg.channel_id.say(val) {
+                if let Err(why) = msg.channel_id.say(&context.http, val).await {
                     log!("Error sending message: {:?}", why);
                 }
-                log!("OK  :[{}] | user:[{}] | chan:{}", msg.content, msg.author.name, msg.channel_id);
-            },
+                log!(
+                    "OK  :[{}] | user:[{}] | chan:{}",
+                    msg.content,
+                    msg.author.name,
+                    msg.channel_id
+                );
+            }
         }
     }
 
     // In this case, just print what the current user's username is.
-    fn on_ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, _: Context, ready: Ready) {
         log!("{} is connected!", ready.user.name);
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
+    let intents = GatewayIntents::non_privileged()
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES;
     // Create a new instance of the Client, logging in as a bot. This will
     // automatically prepend your bot token with "Bot ", which is a requirement
     // by Discord for bot users.
-    let mut client = Client::new(&token, Handler);
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
-    if let Err(why) = client.start() {
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
         log!("Client error: {:?}", why);
     }
 }
